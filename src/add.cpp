752a0b22a8f4370340c47f98d2128ad6ed0afb61
lo7czek <lo7czek@gmail.com>
2019-03-05 20:45:24 +0100

    Added comments and headline

--- src/add.cpp
+++ src/add.cpp
@@ -1,8 +1,15 @@
+/*Katarzyna Kulpa 2i5
+Adding two numbers
+mgr in¿. Wiktor Kuœmirek*/
 #include <iostream>
 using namespace std;
 int main(){
     int number1, number2;
-    cin>>number1>>number2;
+    cout<<"First number: ";
+    cin>>number1;
+    cout<<"Second number: ";
+    cin>>number2;
     int sum = number1 + number2;
+    cout<<"Result: ";
     cout<<sum;
 }
